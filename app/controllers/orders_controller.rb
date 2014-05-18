bbcb75ffaf397358c6ec45041e5dca56e101c8b7
ChrisLangsford <langz020@gmail.com>
2014-05-18 11:05:55 +0200

    realtionship between customer, order and appointment now working, indices displaying correct names

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -56,7 +56,7 @@ class OrdersController < ApplicationController
   def destroy
     @order.destroy
     respond_to do |format|
-      format.html { redirect_to orders_url, notice: 'Order was successfully destroyed.' }
+      format.html { redirect_to orders_url, notice: 'Order was successfully closed.' }
       format.json { head :no_content }
     end
   end
