9c77e4ff50197490ad270270b50c0078f231d21e
ChrisLangsford <langz020@gmail.com>
2014-06-30 10:09:41 +0200

    quote email functionality added

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -5,6 +5,9 @@ class QuotesController < ApplicationController
   def index
   	@customer = Customer.find(@order.customer_id)
   	@garments = @order.garments
+
+    QuoteMailer.quote_email(@customer, @order).deliver
+    
   end
 
   private
