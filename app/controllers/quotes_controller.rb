9784c6577ce0c2890bed62f2287643049ccdd2e0
ChrisLangsford <langz020@gmail.com>
2014-07-09 13:34:31 +0200

    added whenever gem, styling and totals added to correspondence

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,6 +1,7 @@
 class QuotesController < ApplicationController
 	layout "corres"	
-	before_action :get_order, only: [:show]
+	before_action :get_order, only: [:show, :calculate_total]
+  helper_method :calculate_total
 
   def show
   	@customer = Customer.find(@order.customer_id)
@@ -8,6 +9,18 @@ class QuotesController < ApplicationController
     if params[:client] != @customer.first_name
       QuoteMailer.quote_email(@customer, @order, order_quote_url(@order, "1", {client: @customer.first_name})).deliver    
     end
+  end
+
+  def calculate_total
+    total = 0
+    sub_totals =[]
+
+    @order.garments.each do |g|
+       recent = g.costings.last
+       value=recent.labour_cost + recent.fabric_cost + recent.acc_cost + recent.misc_cost
+       sub_totals.push(value)
+    end
+    return total = sub_totals.reduce(:+)    
   end
 
   private
