26c6d4c650d943bd89ad89f166743e99ad1cf5c1
ChrisLangsford <langz020@gmail.com>
2014-07-22 16:12:17 +0200

    assets precompiled for deployment, customer validation improved

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,10 +1,24 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not a valid email address")
+    end
+  end
+end
 class Customer < ActiveRecord::Base
 	has_many :appointments
 	has_many :orders
 
 	validates :first_name,
 			  :last_name,
-			  :email,
 			  :contact_no, presence: true
 
+	validates :email, presence: true, email: true
+
+	validates :first_name, :uniqueness => {scope: :last_name,
+	message: " last name combination has already been taken"}
 end
+
+
+
+
