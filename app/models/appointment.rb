ddfe0300ba78862182e1d57cdaed21bea8f55638
ChrisLangsford <langz020@gmail.com>
2014-07-22 22:28:12 +0200

    two appointments validated to not be scheduled at the same time

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,10 @@ class Appointment < ActiveRecord::Base
   belongs_to :customer
 
   validates :comments, presence: true
+
+  validates :time,
+  			:date, :uniqueness => {:scope => [:date, :time],
+  				message: "is already booked"}
+
+
 end
